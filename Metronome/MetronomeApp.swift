02faaad10de7cfd97c073e8ccd58c12f731c1b1b
Ran Zabaro <ranzabaro@gmail.com>
2025-03-30 23:08:17 +0300

    Initial Commit

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Ran Zabaro on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetronomeTests/MetronomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeTests.swift
+//  MetronomeTests
+//
+//  Created by Ran Zabaro on 30/03/2025.
+//
+
+import Testing
+@testable import Metronome
+
+struct MetronomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
